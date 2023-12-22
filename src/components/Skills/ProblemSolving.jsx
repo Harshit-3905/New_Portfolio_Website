75a8400a1ca8be076bc9c94b99a7e08b82c68a69
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-22 19:57:31 +0530

    Update ProblemSolving component with new skill blocks

--- src/components/Skills/ProblemSolving.jsx
+++ src/components/Skills/ProblemSolving.jsx
@@ -1,10 +1,25 @@
-import { Flex, Heading, VStack } from "@chakra-ui/react";
+import { Wrap, Heading, VStack, Link } from "@chakra-ui/react";
+import SkillBlock from "./SkillBlock";
+import { LeetCodeIcon, GFGIcon, CodechefIcon, CodeforcesIcon } from "./Icons";
 
 const ProblemSolving = () => {
   return (
-    <VStack>
-      <Heading>Frontend</Heading>
-      <Flex></Flex>
+    <VStack height="300px" width="400px" backgroundColor="blue.400">
+      <Heading>Problem Solving</Heading>
+      <Wrap justify="center">
+        <Link href="">
+          <SkillBlock icon={LeetCodeIcon} name="LeetCode" />
+        </Link>
+        <Link href="">
+          <SkillBlock icon={GFGIcon} name="GeeksForGeeks" />
+        </Link>
+        <Link href="">
+          <SkillBlock icon={CodeforcesIcon} name="Codeforces" />
+        </Link>
+        <Link href="">
+          <SkillBlock icon={CodechefIcon} name="Codechef" />
+        </Link>
+      </Wrap>
     </VStack>
   );
 };
