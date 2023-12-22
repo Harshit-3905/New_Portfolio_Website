304c0db177e60f65d79124b24a7612fd4de4d729
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-22 19:57:27 +0530

    Update Others component with new skills

--- src/components/Skills/Others.jsx
+++ src/components/Skills/Others.jsx
@@ -1,10 +1,24 @@
-import { Flex, Heading, VStack } from "@chakra-ui/react";
+import { Wrap, Heading, VStack } from "@chakra-ui/react";
+import SkillBlock from "./SkillBlock";
+import {
+  GitIcon,
+  GithubIcon,
+  VSCodeIcon,
+  NetlifyIcon,
+  PostmanIcon,
+} from "./Icons";
 
 const Others = () => {
   return (
-    <VStack>
+    <VStack height="300px" width="400px" backgroundColor="blue.400">
       <Heading>Others</Heading>
-      <Flex></Flex>
+      <Wrap justify="center">
+        <SkillBlock icon={GitIcon} name="Git" />
+        <SkillBlock icon={GithubIcon} name="GitHub" />
+        <SkillBlock icon={VSCodeIcon} name="VS Code" />
+        <SkillBlock icon={NetlifyIcon} name="Netlify" />
+        <SkillBlock icon={PostmanIcon} name="Postman" />
+      </Wrap>
     </VStack>
   );
 };
