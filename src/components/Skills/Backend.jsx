f4452a26da26d8ad651373fa3b03473016e3a99a
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-22 19:57:06 +0530

    Update backend skills component

--- src/components/Skills/Backend.jsx
+++ src/components/Skills/Backend.jsx
@@ -1,10 +1,17 @@
-import { Heading, VStack, Flex } from "@chakra-ui/react";
+import { Heading, VStack, Wrap } from "@chakra-ui/react";
+import SkillBlock from "./SkillBlock";
+import { ExpressIcon, NodeIcon, MongoIcon } from "./Icons";
 
 const Backend = () => {
   return (
-    <VStack>
+    <VStack height="300px" width="400px" backgroundColor="blue.400">
       <Heading>Backend</Heading>
-      <Flex></Flex>
+      <Wrap justify="center">
+        <SkillBlock icon={NodeIcon} name="Node JS" />
+        <SkillBlock icon={ExpressIcon} name="Express JS" />
+        <SkillBlock icon={MongoIcon} name="MongoDB" />
+        <SkillBlock icon={ExpressIcon} name="ExpressJS" />
+      </Wrap>
     </VStack>
   );
 };
