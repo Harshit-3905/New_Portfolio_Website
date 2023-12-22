a2f68bbc044eb8e2676d5461a34018edee35af2f
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-22 19:57:36 +0530

    Add styling to SkillBlock component

--- src/components/Skills/SkillBlock.jsx
+++ src/components/Skills/SkillBlock.jsx
@@ -3,7 +3,13 @@ import PropTypes from "prop-types";
 
 const SkillBlock = ({ icon: Icon, name }) => {
   return (
-    <HStack>
+    <HStack
+      height="50px"
+      width="150px"
+      backgroundColor="red.300"
+      borderRadius="20px"
+      justifyContent="center"
+    >
       <Icon />
       <Text>{name}</Text>
     </HStack>
