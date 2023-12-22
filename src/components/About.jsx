adc262842fdc0450a96689828af07c7d1a1a9080
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-22 20:38:22 +0530

    Add additional information to the About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,29 @@
-import { VStack, HStack, Heading } from "@chakra-ui/react";
+import { VStack, HStack, Heading, Box, Text } from "@chakra-ui/react";
 
 const About = () => {
   return (
     <VStack id="about" width="80%" height="90vh">
       <Heading>About</Heading>
-      <HStack></HStack>
+      <HStack width="100%">
+        <Box width="50%"></Box>
+        <VStack width="50%">
+          <Text>I'm Harshit</Text>
+          <Text>Passionate Developer</Text>
+          <Text>
+            Third-Year IT Undergraduate Student At Kalyani Government
+            Engineering College. Desire To Make A Positive Impact On The People
+            Around Me And Hope To Achieve This Through Personal Projects,
+            Internships, Hackathons And Other Experiences
+          </Text>
+          <Text>
+            I Am A Self-Taught Developer With A Passion For Web Development And
+            Problem Solving. I Have A Strong Background In Software Development
+            And Have Worked On A Wide Variety Of Projects
+          </Text>
+          <Text>Email : its.harshit.joshi.official@gmail.com</Text>
+          <Text>College : Kalyani Government Engineering College </Text>
+        </VStack>
+      </HStack>
     </VStack>
   );
 };
