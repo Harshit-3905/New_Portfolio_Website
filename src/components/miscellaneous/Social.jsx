083fc9945c372f2f3fe12804d477ccfbbdbec7e6
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-26 00:13:30 +0530

    Refactor Social component to use Flex instead of Box

--- src/components/miscellaneous/Social.jsx
+++ src/components/miscellaneous/Social.jsx
@@ -1,7 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import PropTypes from "prop-types";
 
-const Social = () => {
-  return <Box></Box>;
+const Social = ({ icon: Icon }) => {
+  return (
+    <Flex
+      backgroundColor="red.500"
+      borderRadius="full"
+      height="60px"
+      width="60px"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Icon />
+    </Flex>
+  );
+};
+
+Social.propTypes = {
+  icon: PropTypes.elementType.isRequired,
 };
 
 export default Social;
