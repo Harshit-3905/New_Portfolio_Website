2914762af767a2d0dd01f0fbc5c03d18524a36da
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-25 18:54:48 +0530

    Refactor ProjectCard component to include additional features and improve UI

--- src/components/miscellaneous/ProjectCard.jsx
+++ src/components/miscellaneous/ProjectCard.jsx
@@ -1,20 +1,49 @@
-import { HStack, Image, VStack, Heading, Text, Button } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  VStack,
+  Heading,
+  Text,
+  Button,
+  Link,
+  Icon,
+} from "@chakra-ui/react";
 import PropTypes from "prop-types";
 
 const ProjectCard = (props) => {
   return (
-    <HStack width="100%">
-      <Image width="50%" src={props.image}></Image>
-      <VStack width="50%">
-        <Heading>{props.name}</Heading>
-        <Text>{props.description}</Text>
-        <HStack></HStack>
-        <HStack>
-          <Button>Live</Button>
-          <Button>GitHub</Button>
-        </HStack>
-      </VStack>
-    </HStack>
+    <VStack
+      width="100%"
+      minH="450px"
+      backgroundColor="green.500"
+      borderRadius="2xl"
+      padding={3}
+      justifyContent="center"
+    >
+      <Heading fontSize="30px" textAlign="center">
+        {props.name}
+      </Heading>
+      <HStack pt={5} gap={5}>
+        <Image src={props.image} height="300px"></Image>
+        <VStack gap={3} alignItems="flex-start">
+          <Text>{props.description}</Text>
+          <Text fontSize="20px">Tech Stack :</Text>
+          <HStack>
+            {props.techStack.map((tech) => (
+              <Icon as={tech} key={tech} />
+            ))}
+          </HStack>
+          <HStack pt={3}>
+            <Link href={props.liveLink} isExternal>
+              <Button>Live</Button>
+            </Link>
+            <Link href={props.githubLink} isExternal>
+              <Button>Github</Button>
+            </Link>
+          </HStack>
+        </VStack>
+      </HStack>
+    </VStack>
   );
 };
 
@@ -22,6 +51,9 @@ ProjectCard.propTypes = {
   image: PropTypes.node.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  techStack: PropTypes.array.isRequired,
+  liveLink: PropTypes.string,
+  githubLink: PropTypes.string,
 };
 
 export default ProjectCard;
