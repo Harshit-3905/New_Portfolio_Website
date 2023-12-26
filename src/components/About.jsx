48ab3801d23231bb1995095b629e8e2d776c9e4b
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-26 10:35:44 +0530

    Add image and update About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,42 @@
-import { VStack, HStack, Heading, Box, Text } from "@chakra-ui/react";
+import { VStack, HStack, Heading, Flex, Text, Image } from "@chakra-ui/react";
+import MyImage from "../assets/HarshitImage.jpeg";
 
 const About = () => {
   return (
-    <VStack id="about" width="80%" height="90vh">
-      <Heading>About</Heading>
-      <HStack width="100%">
-        <Box width="50%"></Box>
-        <VStack width="50%">
-          <Text>I'm Harshit</Text>
-          <Text>Passionate Developer</Text>
-          <Text>
-            Third-Year IT Undergraduate Student At Kalyani Government
-            Engineering College. Desire To Make A Positive Impact On The People
-            Around Me And Hope To Achieve This Through Personal Projects,
-            Internships, Hackathons And Other Experiences
+    <VStack id="about" width="80%" pt={10}>
+      <Heading pt={10}>About</Heading>
+      <HStack width="100%" py={20}>
+        <Flex width="50%" alignItems="center" justifyContent="center">
+          <Image
+            src={MyImage}
+            alt="My Image"
+            width="300"
+            height="300"
+            borderRadius="full"
+          />
+        </Flex>
+        <VStack width="50%" textAlign="left" padding={6} borderRadius="lg">
+          <Text fontWeight="bold" fontSize="xl">
+            Hi, I&apos;m Harshit!
           </Text>
-          <Text>
-            I Am A Self-Taught Developer With A Passion For Web Development And
-            Problem Solving. I Have A Strong Background In Software Development
-            And Have Worked On A Wide Variety Of Projects
+          <Text fontSize="lg">
+            I am a passionate developer and a third-year Information Technology
+            undergraduate student at Kalyani Government Engineering College.
           </Text>
-          <Text>Email : its.harshit.joshi.official@gmail.com</Text>
-          <Text>College : Kalyani Government Engineering College </Text>
+          <Text fontSize="lg">
+            My aspiration is to make a positive impact on the people around me
+            through personal projects, internships, hackathons, and various
+            experiences.
+          </Text>
+          <Text fontSize="lg">
+            As a self-taught developer, I am deeply passionate about web
+            development and problem-solving. My strong background in software
+            development has led me to work on a diverse range of projects.
+          </Text>
+          <Text fontSize="lg">
+            Feel free to reach out to me at{" "}
+            <strong>harshitjoshidev@gmail.com</strong>.
+          </Text>
         </VStack>
       </HStack>
     </VStack>
