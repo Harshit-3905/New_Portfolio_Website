bcabfd6346d44cee6a2a5431f38493f41c4e2d5b
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-26 10:35:31 +0530

    Add contact form and image to Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,29 +2,68 @@ import {
   VStack,
   HStack,
   Heading,
-  Image,
   FormControl,
   FormLabel,
   Input,
+  Button,
+  Image,
+  Flex,
+  Textarea,
+  Box,
 } from "@chakra-ui/react";
+import ContactImage from "../assets/ContactImage.png";
 
 const Contact = () => {
   return (
-    <VStack id="contact" width="80%" height="90vh">
+    <VStack id="contact" width="80%" height="90vh" pt={10}>
       <Heading>Get in Contact</Heading>
-      <HStack>
-        <Image alt="pic"></Image>
-        <VStack>
-          <FormControl>
-            <FormLabel>Name</FormLabel>
-            <Input type="text" />
-            <FormLabel>Email</FormLabel>
-            <Input type="email" />
-            <FormLabel>Phone</FormLabel>
-            <Input type="number" />
-            <FormLabel>Message</FormLabel>
-            <Input type="textarea" />
-          </FormControl>
+      <HStack
+        width="100%"
+        height="500px"
+        mt={10}
+        justifyContent="center"
+        alignItems="center"
+        borderRadius="2xl"
+      >
+        <Flex
+          width="50%"
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Image src={ContactImage}></Image>
+        </Flex>
+        <VStack
+          width="50%"
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Box width="70%">
+            <form>
+              <FormControl marginBottom="4">
+                <FormLabel>Name</FormLabel>
+                <Input type="text" name="name" required />
+              </FormControl>
+
+              <FormControl marginBottom="4">
+                <FormLabel>Email</FormLabel>
+                <Input type="email" name="email" required />
+              </FormControl>
+
+              <FormControl marginBottom="4">
+                <FormLabel>Subject</FormLabel>
+                <Input type="text" name="subject" required />
+              </FormControl>
+
+              <FormControl marginBottom="4">
+                <FormLabel>Message</FormLabel>
+                <Textarea type="textarea" name="message" rows={4} required />
+              </FormControl>
+
+              <Button type="submit">Submit</Button>
+            </form>
+          </Box>
         </VStack>
       </HStack>
     </VStack>
