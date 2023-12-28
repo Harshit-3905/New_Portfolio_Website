aa316a0441a540ade2b9bc1deaf3c89ec0b6cc9d
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-28 14:51:59 +0530

    Add logo to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,48 @@
-import { HStack, Text, Button, Link } from "@chakra-ui/react";
+import { HStack, Text, Button, Link, Image } from "@chakra-ui/react";
+import Logo from "../assets/favicon.png";
 
 const Navbar = () => {
+  const hover = () => {
+    return { color: "#F56E0F" };
+  };
   return (
     <HStack
       height="10%"
-      width="80%"
+      width={{ base: "100%", md: "100%", lg: "80%" }}
       justifyContent="space-between"
-      paddingY={5}
+      padding={5}
       position="fixed"
-      backgroundColor="black"
       zIndex={2}
+      backgroundColor="#262626"
     >
-      <Text> Logo</Text>
-      <HStack width="40%" justifyContent="space-between">
-        <Link href="#home">
+      <Image src={Logo} alt="Logo" height="40px" width="40px"></Image>
+      <HStack
+        width="40%"
+        justifyContent="space-between"
+        display={{ base: "none", md: "flex", lg: "flex" }}
+        gap={5}
+      >
+        <Link href="#home" _hover={hover}>
           <Text fontSize="20px" fontWeight="500">
             Home
           </Text>
         </Link>
-        <Link href="#about">
+        <Link href="#about" _hover={hover}>
           <Text fontSize="20px" fontWeight="500">
             About
           </Text>
         </Link>
-        <Link href="#skills">
+        <Link href="#skills" _hover={hover}>
           <Text fontSize="20px" fontWeight="500">
             Skills
           </Text>
         </Link>
-        <Link href="#projects">
+        <Link href="#projects" _hover={hover}>
           <Text fontSize="20px" fontWeight="500">
             Projects
           </Text>
         </Link>
-        <Link href="#contact">
+        <Link href="#contact" _hover={hover}>
           <Text fontSize="20px" fontWeight="500">
             Contact
           </Text>
