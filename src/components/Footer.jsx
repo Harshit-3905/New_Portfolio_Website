bef3b84be01fa2b13a5aac6cc0924ccfd8c4ae40
Harshit-3905 <harshitjoshileet@gmail.com>
2023-12-29 18:49:32 +0530

    Add social media icons and update footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,65 +1,77 @@
+import { VStack, Text, HStack, Link } from "@chakra-ui/react";
+import { FaRegCopyright } from "react-icons/fa";
+import Social from "./miscellaneous/Social";
 import {
-  Flex,
-  Box,
-  Heading,
-  Text,
-  List,
-  ListItem,
-  Link,
-} from "@chakra-ui/react";
+  GithubIcon,
+  LinkedInIcon,
+  TwitterIcon,
+  MailIcon,
+} from "./miscellaneous/Icons";
 
 const Footer = () => {
+  const hover = () => {
+    return { color: "#F56E0F" };
+  };
   return (
-    <>
-      <Flex
-        direction={{ base: "column", md: "row" }}
-        width="80%"
-        justifyContent="center"
-        backgroundColor="black"
-        color="white"
-        padding="4"
+    <VStack width="80%" py={20}>
+      <Text fontSize="20px" fontWeight="500" color="#F56E0F">
+        Harshit Joshi
+      </Text>
+      <HStack
+        width="40%"
+        justifyContent="space-between"
+        display={{ base: "none", md: "flex", lg: "flex" }}
+        gap={5}
+        pt={5}
       >
-        <Box width={{ base: "100%", md: "30%" }} textAlign="center">
-          <Heading fontSize="lg">Harshit&apos;s Portfolio</Heading>
-          <Text>Thank you for visiting my personal portfolio website</Text>
-          <Text>Connect with me over socials.</Text>
-          <Text>
-            Please wait for a while; all projects will be listed soon.
+        <Link href="#home" _hover={hover}>
+          <Text fontSize="18px" fontWeight="400">
+            Home
           </Text>
-        </Box>
-        <Box width={{ base: "100%", md: "30%" }} textAlign="center">
-          <Heading fontSize="lg">Quick Links</Heading>
-          <List styleType="none" id="links">
-            <ListItem>
-              <Link href="#home">Home</Link>
-            </ListItem>
-            <ListItem>
-              <Link href="#about">About</Link>
-            </ListItem>
-            <ListItem>
-              <Link href="#skills">Skills</Link>
-            </ListItem>
-            <ListItem>
-              <Link href="#project">Projects</Link>
-            </ListItem>
-            <ListItem>
-              <Link href="#contact">Contact</Link>
-            </ListItem>
-          </List>
-        </Box>
-        <Box width={{ base: "100%", md: "30%" }} textAlign="center">
-          <Heading fontSize="lg">Contact Info</Heading>
-          <Text>harshitjoshidev@gmail.com</Text>
-          <Text>Kolkata, West Bengal</Text>
-        </Box>
-      </Flex>
-      <Box textAlign="center" color="white" padding="4">
-        Made by &nbsp;
-        <Link href="https://www.linkedin.com/in/harshit-joshi-40953321b/">
-          Harshit Joshi
         </Link>
-      </Box>
-    </>
+        <Link href="#about" _hover={hover}>
+          <Text fontSize="18px" fontWeight="400">
+            About
+          </Text>
+        </Link>
+        <Link href="#skills" _hover={hover}>
+          <Text fontSize="18px" fontWeight="400">
+            Skills
+          </Text>
+        </Link>
+        <Link href="#projects" _hover={hover}>
+          <Text fontSize="18px" fontWeight="400">
+            Projects
+          </Text>
+        </Link>
+        <Link href="#contact" _hover={hover}>
+          <Text fontSize="18px" fontWeight="400">
+            Contact
+          </Text>
+        </Link>
+      </HStack>
+      <HStack pt={5} gap={3}>
+        <Link
+          href="https://www.linkedin.com/in/harshit-joshi-40953321b/"
+          isExternal
+        >
+          <Social icon={LinkedInIcon} />
+        </Link>
+        <Link href="https://github.com/Harshit-3905" isExternal>
+          <Social icon={GithubIcon} />
+        </Link>
+        <Link href="https://twitter.com/Harshit3905" isExternal>
+          <Social icon={TwitterIcon} />
+        </Link>
+        <Link href="#contact">
+          <Social icon={MailIcon} />
+        </Link>
+      </HStack>
+      <HStack pt={10}>
+        <FaRegCopyright />
+        <Text fontSize="15px">2024 Harshit Joshi. All rights reserved.</Text>
+      </HStack>
+    </VStack>
   );
 };
 
