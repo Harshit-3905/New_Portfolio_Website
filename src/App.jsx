88281ea29f2090eb8003f7de0a516d7f8009705a
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-24 19:58:22 +0530

    Refactor App component and add loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,48 @@
 import "./App.css";
 import { VStack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import HireMe from "./components/HireMe";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import PreLoader from "./components/PreLoader";
 import Starfield from "react-starfield";
-import { Suspense, lazy } from "react";
-import Home from "./components/Home";
-
-const Navbar = lazy(() => import("./components/Navbar"));
-const About = lazy(() => import("./components/About"));
-const Skills = lazy(() => import("./components/Skills"));
-const Projects = lazy(() => import("./components/Projects"));
-const HireMe = lazy(() => import("./components/HireMe"));
-const Contact = lazy(() => import("./components/Contact"));
-const Footer = lazy(() => import("./components/Footer"));
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  if (loading) return <PreLoader />;
   return (
-    <Suspense fallback={<PreLoader />}>
-      <VStack
-        width="100%"
-        alignItems="center"
+    <VStack
+      width="100%"
+      alignItems="center"
+      backgroundColor="black"
+      color="white"
+      overflowX="hidden"
+    >
+      <Starfield
+        starCount={2000}
+        starColor={[255, 255, 255]}
+        speedFactor={0.07}
         backgroundColor="black"
-        color="white"
-        overflowX="hidden"
-      >
-        <Starfield
-          starCount={2000}
-          starColor={[255, 255, 255]}
-          speedFactor={0.07}
-          backgroundColor="black"
-        />
-        <Navbar />
-        <Home />
-        <About />
-        <Skills />
-        <Projects />
-        <HireMe />
-        <Contact />
-        <Footer />
-      </VStack>
-    </Suspense>
+      />
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <HireMe />
+      <Contact />
+      <Footer />
+    </VStack>
   );
 }
 
