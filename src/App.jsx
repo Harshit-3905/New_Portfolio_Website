97f026327337b31668f93231b905ceaff803c26d
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-24 19:40:23 +0530

    Refactor code to use lazy loading and add PreLoader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
 import "./App.css";
 import { VStack } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import HireMe from "./components/HireMe";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import PreLoader from "./components/PreLoader";
 import Starfield from "react-starfield";
+import { Suspense, lazy } from "react";
+
+const Navbar = lazy(() => import("./components/Navbar"));
+const Home = lazy(() => import("./components/Home"));
+const About = lazy(() => import("./components/About"));
+const Skills = lazy(() => import("./components/Skills"));
+const Projects = lazy(() => import("./components/Projects"));
+const HireMe = lazy(() => import("./components/HireMe"));
+const Contact = lazy(() => import("./components/Contact"));
+const Footer = lazy(() => import("./components/Footer"));
 
 function App() {
   return (
-    <VStack
-      width="100%"
-      alignItems="center"
-      backgroundColor="black"
-      color="white"
-      overflowX="hidden"
-    >
-      <Starfield
-        starCount={2000}
-        starColor={[255, 255, 255]}
-        speedFactor={0.07}
+    <Suspense fallback={<PreLoader />}>
+      <VStack
+        width="100%"
+        alignItems="center"
         backgroundColor="black"
-      />
-      <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-      <HireMe />
-      <Contact />
-      <Footer />
-    </VStack>
+        color="white"
+        overflowX="hidden"
+      >
+        <Starfield
+          starCount={2000}
+          starColor={[255, 255, 255]}
+          speedFactor={0.07}
+          backgroundColor="black"
+        />
+        <Navbar />
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <HireMe />
+        <Contact />
+        <Footer />
+      </VStack>
+    </Suspense>
   );
 }
 
