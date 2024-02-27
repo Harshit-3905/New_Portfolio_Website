e8347250c48bb34f37c6267413f80c7da0ced9d7
Harshit-3905 <harshitjoshileet@gmail.com>
2024-02-27 15:40:33 +0530

    Update Navbar component with new icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import { HStack, Text, Button, Link, Image } from "@chakra-ui/react";
+import { HStack, Text, Link, Image } from "@chakra-ui/react";
 import Logo from "../assets/favicon.webp";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
 import { Fade } from "react-awesome-reveal";
+import { FaHome, FaUser, FaCode } from "react-icons/fa";
+import { IoCall } from "react-icons/io5";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
 const Navbar = () => {
   return (
@@ -23,47 +25,46 @@ const Navbar = () => {
           gap={10}
         >
           <Link href="#home" _hover={{ color: "#F56E0F" }}>
-            <Text fontSize="22px" fontWeight="500">
-              Home
-            </Text>
+            <HStack>
+              <FaHome size="25px" />
+              <Text fontSize="22px" fontWeight="500">
+                Home
+              </Text>
+            </HStack>
           </Link>
           <Link href="#about" _hover={{ color: "#F56E0F" }}>
-            <Text fontSize="22px" fontWeight="500">
-              About
-            </Text>
+            <HStack>
+              <FaUser size="20px" />
+              <Text fontSize="22px" fontWeight="500">
+                About
+              </Text>
+            </HStack>
           </Link>
           <Link href="#skills" _hover={{ color: "#F56E0F" }}>
-            <Text fontSize="22px" fontWeight="500">
-              Skills
-            </Text>
+            <HStack>
+              <FaCode size="25px" />
+              <Text fontSize="22px" fontWeight="500">
+                Skills
+              </Text>
+            </HStack>
           </Link>
           <Link href="#projects" _hover={{ color: "#F56E0F" }}>
-            <Text fontSize="22px" fontWeight="500">
-              Projects
-            </Text>
+            <HStack>
+              <AiOutlineFundProjectionScreen size="25px" />
+              <Text fontSize="22px" fontWeight="500">
+                Projects
+              </Text>
+            </HStack>
           </Link>
           <Link href="#contact" _hover={{ color: "#F56E0F" }}>
-            <Text fontSize="22px" fontWeight="500">
-              Contact
-            </Text>
+            <HStack gap={1}>
+              <IoCall size="25px" />
+              <Text fontSize="22px" fontWeight="500">
+                Contact
+              </Text>
+            </HStack>
           </Link>
         </HStack>
-        {/* <Link
-          href="https://drive.google.com/file/d/1k3ept5svEKYSsri7GLkIaHmsWGHyNNyl/view?usp=sharing"
-          isExternal
-        >
-          <Button
-            rightIcon={<ArrowForwardIcon />}
-            backgroundColor="#F56E0F"
-            borderRadius="xl"
-            border="1px solid #F56E0F"
-            _hover={{ color: "#F56E0F", backgroundColor: "#262626" }}
-            height="38px"
-            fontSize={{ base: "12px", md: "16px" }}
-          >
-            Resume
-          </Button>
-        </Link> */}
       </Fade>
     </HStack>
   );
