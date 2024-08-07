e771a0e8adba0cf3a4d42f810e1d03c36e002451
Harshit Joshi <harshitjoshileet@gmail.com>
2024-08-07 10:46:46 +0530

    Lazy load components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,45 @@
 import "./App.css";
 import { VStack } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import HireMe from "./components/HireMe";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Github from "./components/Github";
 import PreLoader from "./components/PreLoader";
 import Starfield from "react-starfield";
-import { useState, useEffect } from "react";
+import { Suspense, lazy } from "react";
+const Home = lazy(() => import("./components/Home"));
+const Navbar = lazy(() => import("./components/Navbar"));
+const About = lazy(() => import("./components/About"));
+const Skills = lazy(() => import("./components/Skills"));
+const Projects = lazy(() => import("./components/Projects"));
+const HireMe = lazy(() => import("./components/HireMe"));
+const Contact = lazy(() => import("./components/Contact"));
+const Footer = lazy(() => import("./components/Footer"));
+const Github = lazy(() => import("./components/Github"));
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-  if (loading) return <PreLoader />;
   return (
-    <VStack
-      width="100%"
-      alignItems="center"
-      backgroundColor="black"
-      color="white"
-      overflowX="hidden"
-    >
-      <Starfield
-        starCount={2000}
-        starColor={[255, 255, 255]}
-        speedFactor={0.07}
+    <Suspense fallback={<PreLoader />}>
+      <VStack
+        width="100%"
+        alignItems="center"
         backgroundColor="black"
-      />
-      <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Github />
-      <Projects />
-      <HireMe />
-      <Contact />
-      <Footer />
-    </VStack>
+        color="white"
+        overflowX="hidden"
+      >
+        <Starfield
+          starCount={2000}
+          starColor={[255, 255, 255]}
+          speedFactor={0.07}
+          backgroundColor="black"
+        />
+        <Navbar />
+        <Home />
+        <About />
+        <Skills />
+        <Github />
+        <Projects />
+        <HireMe />
+        <Contact />
+        <Footer />
+      </VStack>
+    </Suspense>
   );
 }
 
